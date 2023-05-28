135429d7a85405cd5cebaf314b4a9cd95d789ecb
Rovshen Annayev <81748081+rovshena@users.noreply.github.com>
2023-05-28 15:43:15 +0500

    site assets

---
+++ public/assets/js/layout.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", function () {
+    $('.preloader').fadeOut();
+});
+
+function disableSubmitButton() {
+    document.getElementById('submit-button').setAttribute('disabled', 'true');
+    document.getElementById('loading').classList.remove('d-none');
+    $('.preloader').show();
+}
