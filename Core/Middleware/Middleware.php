385d34f59f12baf7f4665cec06243522306d7b01
Rovshen Annayev <81748081+rovshena@users.noreply.github.com>
2023-05-28 11:42:39 +0500

    initial commit

---
+++ Core/Middleware/Middleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Core\Middleware;
+
+use Exception;
+
+class Middleware
+{
+    public const MAP = [
+        'guest' => Guest::class,
+        'auth' => Authenticated::class
+    ];
+
+    /**
+     * @throws Exception
+     */
+    public static function resolve($key)
+    {
+        if (!$key) {
+            return;
+        }
+
+        $middleware = static::MAP[$key] ?? false;
+
+        if (!$middleware) {
+            throw new Exception("No matching middleware found for key '{$key}'.");
+        }
+
+        (new $middleware)->handle();
+    }
+}
